ed0584d03fea1de137b24e4cf7aa7dfba1e75cfa
Matt M. <matt.merrill@taxworks.com>
2013-03-01 17:04:29 -0700

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCParse.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public async Task<ActionResult> Index()
+        {
+          var testObject = new ParseObject("TestObject");
+          testObject["foo"] = "fighters";
+          await testObject.SaveAsync();
+
+          return View();
+        }
+
+    }
+}
